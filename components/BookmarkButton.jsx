2577cafbe18877a31f7eb1bbdf60bd3f0b82aecc
Mykhailo Lebediev <mykhailo.lebedev@codeandtheory.com>
2025-10-06 11:56:37 +0300

    NEXTJS: bookmark functionality added + saved properties page

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,8 +1,63 @@
+'use client'
+import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { FaBookmark } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const BookmarkButton = ({ property }) => {
-    return ( 
+    const {data:session} = useSession();
+    const userId = session?.user?.id;
+
+    const [isBookmarked, setIsBookmarked] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    // FIXME:TODO: read about useEffect
+    useEffect(() => {
+        if (!userId) {
+            setLoading(false)
+            return;
+        } else {
+            // FIXME:TODO: read about then when to use
+            checkBookmarkStatus(property._id).then((res) => {
+                if (res.error) toast.error(res.error);
+                if (res.isBookmarked) setIsBookmarked(true);
+                setLoading(false);
+            })
+        }
+    }, [property._id, userId, checkBookmarkStatus]);
+
+    const handleCLick = async () => {
+        if (!userId) {
+            toast.error('You need to be signed in to bookmark listing');
+            return;
+        }
+
+        bookmarkProperty(property._id).then((res) => {
+            if (res.error) {
+                toast.error(res.error);
+            } else {
+                setIsBookmarked(res.isBookmarked);
+                toast.success(res.message);
+            }
+        })
+    }
+
+    // FIXME:TODO: add loading state
+    if (loading) {
+        return <p className="text-center">Loading...</p>
+    }
+    return isBookmarked ? ( 
         <button
+            onClick={handleCLick}
+            className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+        >
+            <FaBookmark className="mr-2" /> Remove Bookmark
+        </button>
+    ) : (
+        <button
+            onClick={handleCLick}
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
         >
             <FaBookmark className="mr-2" /> Bookmark Property
--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -1,6 +1,5 @@
 import { FaPaperPlane } from "react-icons/fa";
 
-
 const PropertyContactForm = ({ property }) => {
     return ( 
         <div className="bg-white p-6 rounded-lg shadow-md">
@@ -9,69 +8,69 @@ const PropertyContactForm = ({ property }) => {
             <form>
                 <div className="mb-4">
                     <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="name"
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="name"
                     >
-                    Name:
+                        Name:
                     </label>
                     <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="Enter your name"
-                    required
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Enter your name"
+                        required
                     />
                 </div>
                 <div className="mb-4">
                     <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="email"
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="email"
                     >
-                    Email:
+                        Email:
                     </label>
                     <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Enter your email"
-                    required
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Enter your email"
+                        required
                     />
                 </div>
                 <div className="mb-4">
                     <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="phone"
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="phone"
                     >
-                    Phone:
+                        Phone:
                     </label>
                     <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="phone"
-                    name="phone"
-                    type="text"
-                    placeholder="Enter your phone number"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="phone"
+                        name="phone"
+                        type="text"
+                        placeholder="Enter your phone number"
                     />
                 </div>
                 <div className="mb-4">
                     <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="message"
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="message"
                     >
-                    Message:
+                        Message:
                     </label>
                     <textarea
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-                    id="message"
-                    name="message"
-                    placeholder="Enter your message"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
+                        id="message"
+                        name="message"
+                        placeholder="Enter your message"
                     ></textarea>
                 </div>
                 <div>
                     <button
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
-                    type="submit"
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
+                        type="submit"
                     >
                         <FaPaperPlane className="mr-2" /> Send Message
                     </button>
