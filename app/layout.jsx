d65ee199c88d4a9eefd476c4f7509a80c61d2d49
Mykhailo Lebediev <mykhailo.lebedev@codeandtheory.com>
2025-10-10 12:39:35 +0300

    NEXTJS: Messages: add/delete + global context for counter

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import AuthProvider from '@/components/AuthProvider';
 import '@assets/styles/globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { GlobalProvider } from '@/context/GlobalContext';
 
 
 export const metadata = {
@@ -16,16 +17,18 @@ export const metadata = {
 const MainLayout = ({ children }) => {
     return (
         <AuthProvider>
-            <html >
-                <body>
-                    <Navbar/>
-                    <main>
-                        { children }
-                    </main>
-                    <Footer/>
-                    <ToastContainer />
-                </body>
-            </html>
+            <GlobalProvider>
+                <html >
+                    <body>
+                        <Navbar/>
+                        <main>
+                            { children }
+                        </main>
+                        <Footer/>
+                        <ToastContainer />
+                    </body>
+                </html>
+            </GlobalProvider>
         </AuthProvider>
     );
 }
