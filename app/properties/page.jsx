b5ebe1c9d6017b57691f5cc908845f3722cee1f3
Mykhailo Lebediev <mykhailo.lebedev@codeandtheory.com>
2025-10-10 13:18:37 +0300

    NEXTJS: Pagination

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,16 +1,25 @@
+import Pagination from '@/components/Pagination';
 import PropertyCard from '@/components/PropertyCard';
-// import properties from '@/properties.json'
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({ searchParams }) => {
+    const { page = 1, pageSize = 3 } = await searchParams;
     await connectDB();
-    const properties = await Property.find({ }).lean();
+
+    const skip = (page - 1) * pageSize;
+    const total = await Property.countDocuments({});
+
+    const showPagination = total > pageSize;
+
+    // const properties = await Property.find({ }).lean();
+    const properties = await Property.find({ }).skip(skip).limit(pageSize);
 
     return ( 
         <section className='px-4 py-6'>
             <div className='container-xl lg:container m-auto px-4 py-6'>
                 <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">Recent Properties</h2>
+
                 { properties.length === 0 ? (<p>No properties found</p>) : (
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                         {
@@ -19,6 +28,10 @@ const PropertiesPage = async () => {
                             ))
                         }
                     </div>
+                )}
+
+                {showPagination && (
+                    <Pagination page={parseInt(page)} pageSize={parseInt(pageSize)} totalItems={total} />
                 )}
             </div>
         </section>
